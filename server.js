145568435269f1b26c91bbd3c91323e4327cb7ef
7st5zchchb-collab <7st5zchchb@privaterelay.appleid.com>
2026-09-23 11:29:22 +0400

    Proxy CoinGecko market categories

--- server.js
+++ server.js
@@ -145,6 +145,7 @@ async function coingeckoProxy(req,res,next){
   if(p==="/global") target=COINGECKO_BASE+"/global";
   else if(p==="/global/market_cap_chart") target=COINGECKO_BASE+"/global/market_cap_chart?"+new URLSearchParams(req.query).toString();
   else if(p==="/search") target=COINGECKO_BASE+"/search?"+new URLSearchParams(req.query).toString();
+  else if(p==="/coins/categories") target=COINGECKO_BASE+"/coins/categories?"+new URLSearchParams(req.query).toString();
   else if(p==="/coins/markets") target=COINGECKO_BASE+"/coins/markets?"+new URLSearchParams(req.query).toString();
   else if(p.startsWith("/coins/") && p.endsWith("/market_chart")){
     const id=p.split("/")[2];
